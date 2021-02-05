ec430fb8ef2f0122c8e0c151c15e4f8547eb008f
dabin0808 <1633984208@qq.com>
2021-02-05 16:15:16 +0800

    commit oneTest

--- HelloWorld/src/main/java/com/alipay/demo/config/BaseController.java
+++ HelloWorld/src/main/java/com/alipay/demo/config/BaseController.java
@@ -16,31 +16,32 @@
  */
 package com.alipay.demo.config;
 
+import com.alipay.api.AlipayClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import com.alipay.api.AlipayClient;
 
-/** 控制层基础类，承载公共配置信息
- * @author alipay.demo
+/**
+ * 控制层基础类，承载公共配置信息
  *
+ * @author alipay.demo
  */
 public class BaseController {
     /**
-	 * 配置文件加载
-	 */
-    @Autowired  
+     * 配置文件加载
+     */
+    @Autowired
     protected Environment config;
 
-    @Autowired  
+    @Autowired
     protected AlipayClient alipayClient;
-    
 
-	public void setAlipayClient(AlipayClient alipayClient) {
-		this.alipayClient = alipayClient;
-	}
 
-	public void setConfig(Environment config) {
-		this.config = config;
-	} 
-    
+    public void setAlipayClient(AlipayClient alipayClient) {
+        this.alipayClient = alipayClient;
+    }
+
+    public void setConfig(Environment config) {
+        this.config = config;
+    }
+
 }
